5cb653b099e39cdeac9ee40d5b440b4818274cf1
f552p69 <f552p69@gmail.com>
2023-02-26 10:05:36 +0400

    refactoring according to sonarcloud.io #8

--- n0struct/n0struct_random.py
+++ n0struct/n0struct_random.py
@@ -1,5 +1,13 @@
 import typing
 import random
+import os
+
+try:
+    if __system_random:
+        pass
+except NameError:
+    __system_random = random.SystemRandom()
+
 # ******************************************************************************
 # ******************************************************************************
 def rnd(till_not_included: int) -> int:
@@ -7,7 +15,7 @@ def rnd(till_not_included: int) -> int:
     :param till_not_included:
     :return: int [0..till_not_included)
     """
-    return int(random.random() * till_not_included)
+    return __system_random.randrange(0, till_not_included)
 # ******************************************************************************
 def random_from(from_list: list) -> typing.Any:
     """
